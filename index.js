96b8b7d412726799b6f1eea06e91974176107fcf
Danish <83212657+hellcatWahran@users.noreply.github.com>
2022-01-01 17:26:13 +0530

    uploaded the code

---
+++ index.js
@@ -0,0 +1,8 @@
+var btnCont = document.getElementById("btn-cont");
+var mainCont = document.getElementById("main-cont");
+var h2 = document.getElementById("hny");
+const showMain = () => {
+  btnCont.style.display = "none";
+  mainCont.style.display = "flex";
+};
+
